19d2abe18a910d0ab585897b45e8ea1a9657a616
Vicky Lin <vickyliinn@gmail.com>
2017-09-04 00:54:56 +0800

    add date-picker

---
+++ src/components/date-picker.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-menu :close-on-content-click="true"
+          max-width="290px"
+          offset-y>
+    <v-text-field
+        slot="activator"
+        :label="label"
+        :value="value"
+        prepend-icon="event"
+        readonly
+    ></v-text-field>
+    <v-date-picker :value="value"
+                   @input="e => $emit('input', e)"
+                   no-title
+                   scrollable>
+    </v-date-picker>
+  </v-menu>
+</template>
+
+<script>
+  export default {
+    props: ['value', 'label'],
+  }
+</script>
+
+<style lang="stylus">
+  .application--dark
+  .picker--date__header
+  .picker--date table th
+    color: white
+</style>

0364322f19595f3b97ed72259ac69a1de33d19c9
vickyliin <r05922128@csie.ntu.edu.tw>
2017-09-05 14:31:54 +0800

    Update config.js
    change google client id

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ module.exports = {
   timeLocaleFormat: 'haw-us',
   queryInterval: 300000, //ms
   gAuthSettings: {
-    client_id: '904848968355-qqd08ni3a2pjgfe5hoc53tslnam7t12j.apps.googleusercontent.com',
+    client_id: '128291458390-1rjai5msiieuad8ofmeje5eonoplsmf5.apps.googleusercontent.com',
     cookiepolicy: 'single_host_origin',
   },
   gSuiteDomain: 'nlg.csie.ntu.edu.tw',

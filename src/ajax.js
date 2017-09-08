1d54df8bb9a0660dde41020781d28720cf9b702b
Vicky Lin <vickyliinn@gmail.com>
2017-09-08 21:59:58 +0800

    ajax: change to async

--- src/ajax.js
+++ src/ajax.js
@@ -1,41 +1,38 @@
-function XhrWrapper(opt){
+function XhrWrapper(opt) {
   let xhr = this.xhr = new XMLHttpRequest()
   xhr.responseType = opt.resType || 'json'
   xhr.open(opt.type, opt.url, true)
-  if(opt.contentType) xhr.setRequestHeader('Content-type', opt.contentType)
-  xhr.withCredentials = opt.credential === undefined? true: opt.credential
-  this.send = data => new Promise( resolve => {
-    xhr.onreadystatechange = (() => {
-      let {response, status} = xhr
-      if(xhr.readyState === 4){
-        if(opt.ready) opt.ready(response, status)
-        resolve({response, status})
-      }
-    })
+  if (opt.contentType) xhr.setRequestHeader('Content-type', opt.contentType)
+  xhr.withCredentials = opt.credential === undefined ? true : opt.credential
+  this.send = async data => {
+    let stateChange = new Promise(resolve => xhr.onload = resolve)
     xhr.send(data)
-  })
+    await stateChange
+    let { response, status } = xhr
+    if (opt.ready) opt.ready(response, status)
+    return { response, status }
+  }
 }
 export default {
-  params(data){
+  params(data) {
     let parameters
-    if(data !== undefined){
+    if (data !== undefined) {
       parameters = Object.entries(data)
-          .map(pair => `${encodeURI(pair[0])}=${encodeURI(pair[1])}`)
-          .join('&')
-    }
-    else{
+        .map(pair => `${encodeURI(pair[0])}=${encodeURI(pair[1])}`)
+        .join('&')
+    } else {
       parameters = ''
     }
     return parameters
   },
-  get(opt){
+  get(opt) {
     opt.resType = opt.type
     opt.type = 'GET'
     opt.url = `${opt.url}?${this.params(opt.data)}`
     let xhr = new XhrWrapper(opt)
     return xhr.send()
   },
-  post(opt){
+  post(opt) {
     opt.contentType = opt.contentType || 'application/x-www-form-urlencoded'
     opt.resType = opt.type
     opt.type = 'POST'

5075bb42a4525ebfb93cd97d81838e1ffe146082
Vicky Lin <vickyliinn@gmail.com>
2017-09-09 01:27:28 +0800

    actions: format

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,23 +1,23 @@
 import 'assets/js/platform'
-import {gAuthSettings, gClientSettings, entry} from 'config'
+import { gAuthSettings, gClientSettings, entry } from 'config'
 import $ from 'ajax'
 
 const loginUrl = entry + '/login'
 const logoutUrl = entry + '/logout'
 const userUrl = entry + '/user'
 
-const getUserRole = async () =>{
-  let {response: user} = await $.get({url: userUrl})
+const getUserRole = async () => {
+  let { response: user } = await $.get({ url: userUrl })
   return user.role
 }
 
 let authentications = {
-  gLoadAuth(){
+  gLoadAuth() {
     return new Promise(resolve =>
-        gapi.load('client:auth2', resolve)
+      gapi.load('client:auth2', resolve)
     )
   },
-  async gAuthInit({commit, dispatch}){
+  async gAuthInit({ commit, dispatch }) {
     await dispatch('gLoadAuth')
     await gapi.client.init(gClientSettings)
     let gAuth = gapi.auth2.getAuthInstance()
@@ -25,74 +25,74 @@ let authentications = {
     await gAuth.then()
 
     let user = gAuth.currentUser.get()
-    if(user){
+    if (user) {
       commit('user', user)
       commit('userRole', await getUserRole())
     }
 
     return [gAuth]
   },
-  async gSignIn({state: {gAuth}, commit, dispatch}){
-    if(!gapi.auth2){
+  async gSignIn({ state: { gAuth }, commit, dispatch }) {
+    if (!gapi.auth2) {
       gAuth = (await dispatch('gAuthInit'))[0]
     }
-    else{
+    else {
       await gAuth.then()
     }
     let user
-    try{
+    try {
       user = await gAuth.signIn({
         prompt: 'select_account'
       })
     }
-    catch({error}){
+    catch ({ error }) {
       throw Error(`gAuth.signIn Error: ${error}`)
     }
     commit('user', user)
     return user
   },
-  async signIn({dispatch, commit}){
+  async signIn({ dispatch, commit }) {
     let user
-    try{
+    try {
       user = await dispatch('gSignIn')
     }
-    catch(e){
+    catch (e) {
       return
     }
-    let {status} = await $.post({
+    let { status } = await $.post({
       url: loginUrl,
-      data: {id_token: user.getAuthResponse().id_token}
+      data: { id_token: user.getAuthResponse().id_token }
     })
     commit('userRole', await getUserRole())
     commit('status', status)
   },
-  async gSignOut({state: {gAuth}, commit, dispatch}){
-    if(gAuth === null){
+  async gSignOut({ state: { gAuth }, commit, dispatch }) {
+    if (gAuth === null) {
       gAuth = (await dispatch('gAuthInit'))[0]
     }
-    try{
+    try {
       await gAuth.signOut()
     }
-    catch({error}){
+    catch ({ error }) {
       throw Error(`gAuth.signOut Error: ${error}`)
     }
     commit('user', null)
   },
-  async signOut({dispatch, commit}){
+  async signOut({ dispatch, commit }) {
     await dispatch('gSignOut')
-    await $.post({url: logoutUrl})
+    await $.post({ url: logoutUrl })
     commit('status', 401)
   }
 }
 
 export default {
   ...authentications,
-  async getData({commit}, model){
-    let {response, status} = await $.get({
-      url: entry + model
+  async getData({ commit }, model) {
+    let { response, status } = await $.get({
+      url: entry + model,
     })
     commit('status', status)
-    if(status !== 200) return null
+    if (status !== 200) return null
     return response
   }
 }

30ab936a9c94a2c7b08801223c69ba85f38f37d7
Vicky Lin <vickyliinn@gmail.com>
2017-09-10 18:24:17 +0800

    change time locale to zh-tw

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   wsDataEntry: 'http://nlg17.csie.ntu.edu.tw:5566',
   entry: 'http://localhost:3000/api',
   routerMode: 'history',
-  timeLocaleFormat: 'haw-us',
+  timeLocaleFormat: 'zh-tw',
   queryInterval: 300000, //ms
   gAuthSettings: {
     client_id: '128291458390-1rjai5msiieuad8ofmeje5eonoplsmf5.apps.googleusercontent.com',

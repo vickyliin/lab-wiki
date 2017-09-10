98812a0c5abf0cad5ba266cc18c75be75bdef3ae
Vicky Lin <vickyliinn@gmail.com>
2017-09-10 22:37:33 +0800

    add cpu usage

---
+++ src/pages/cpu-usage.vue
@@ -0,0 +1,34 @@
+<template>
+  <workstation-usage
+    :headers="headers"
+    :items-mapper="itemsMapper">
+  </workstation-usage>
+</template>
+
+<script>
+  import workstationUsage from 'components/workstation-usage.vue'
+
+  export default {
+    components: { workstationUsage },
+    data() {
+      return {
+        headers: 'server cpu usage memory'.split(' '),
+        itemsMapper(d){
+          return {
+            server: d.hostname.replace('nlg-wks-', ''),
+            cpu: {
+              display: d.cpuinfo.type
+            },
+            name: d.cpuinfo.type,
+            usage: d.cpu.cpu_usage/d.cpuinfo.threads,
+            memory: this.setMemory({
+              usage: d.cpu.mem_usage,
+              total: d.cpu.mem_total,
+            }),
+            logtime: new Date(d.logtime),
+          }
+        }
+      }
+    },
+  }
+</script>

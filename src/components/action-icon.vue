58d0795623981b6120cde5a9d18a0dc3033cdb69
Vicky Lin <vickyliinn@gmail.com>
2017-09-12 01:25:16 +0800

    add action icon component

---
+++ src/components/action-icon.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-btn icon small
+          v-if="show(item)"
+          :href="href? href(item): ''"
+          @click.stop="action(item)"
+          style="margin: 0"
+          :class="color? color+'--text': ''">
+    <v-icon>{{icon}}</v-icon>
+  </v-btn>
+</template>
+
+<script>
+  export default {
+    props: {
+      item: {
+        type: Object,
+        required: true,
+      },
+      icon: {
+        type: String,
+        required: true,
+      },
+      color: {
+        type: String
+      },
+      show: {
+        type: Function,
+        default: () => true
+      },
+      href: {
+        type: Function,
+      },
+      action: {
+        type: Function,
+      },
+    },
+  }
+</script>

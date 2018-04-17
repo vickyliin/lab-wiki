8dca53404fa116d9c33d6ddd4d74c7dd3b315789
vickyliin <vickyliinn@gmail.com>
2018-04-18 02:02:24 +0800

    update eslint-plugin-vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,14 +13,5 @@ module.exports = {
   globals: {
     gapi: false,
     Chart: false
-  },
-  rules: {
-    'vue/mustache-interpolation-spacing': [2, 'always'],
-    'vue/attribute-hyphenation': [2, 'always'],
-    'vue/name-property-casing': [2, 'kebab-case'],
-    'vue/no-multi-spaces': 2,
-    'vue/v-on-style': [2, 'shorthand'],
-    'vue/html-self-closing': 2,
-    'vue/html-indent': [2, 2]
   }
 };
